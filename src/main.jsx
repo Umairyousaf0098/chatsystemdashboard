2708a76a2f853e1de4280269d413ab8c29e97b0b
Umairyousaf0098 <160516533+Umairyousaf0098@users.noreply.github.com>
2024-04-27 13:12:04 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 
+const YOUR_TRACKING_ID = 'G-6J656MK6WQ';
+
+// Function to initialize Google Analytics
+function initializeAnalytics() {
+  const script = document.createElement('script');
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${YOUR_TRACKING_ID}`;
+  script.async = true;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', YOUR_TRACKING_ID);
+}
+
+// Render the React application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Initialize Google Analytics after rendering the application
+initializeAnalytics();
